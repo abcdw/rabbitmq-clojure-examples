be2153e826449a5bd9f7f7d2d5e0dd893d87349c
abcdw <andrewtropin@gmail.com>
2018-01-05 17:32:27 +0300

    Init the repo with basic clojure files

---
+++ test/rabbitmq_clojure_examples/core_test.clj
@@ -0,0 +1,7 @@
+(ns rabbitmq-clojure-examples.core-test
+  (:require [clojure.test :refer :all]
+            [rabbitmq-clojure-examples.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
